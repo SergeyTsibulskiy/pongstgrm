8ce9eaf29c0f21a1724500a2b7d9c5f26ad5d633
ThibulskiySergey <c4r0n0s@gmail.com>
2016-03-01 19:27:37 +0200

    Update js

--- Gulpfile.js
+++ Gulpfile.js
@@ -7,14 +7,12 @@ var sass   = require('gulp-sass');
 var uglify = require('gulp-uglify');
 
 gulp.task('development', function() {
-  gulp.src('./src/coffee/*.coffee')
-    .pipe(watch('./src/coffee/*.coffee'))
-    .pipe(coffee({ bare: true }))
-    //.pipe(uglify())
-    .pipe(gulp.dest('./dist/source'));
-  //
-  //gulp.src('./src/scss/*.scss')
-  //  .pipe(watch('./src/scss/*.scss'))
-  //  .pipe(sass({ outputStyle: 'expanded' }))
-  //  .pipe(gulp.dest('./dist/css'))
+  gulp.src('./src/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/source-min'));
+
+  gulp.src('./src/scss/*.scss')
+    .pipe(watch('./src/scss/*.scss'))
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(gulp.dest('./dist/css'))
 });

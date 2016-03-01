072f3e4c631ae59a48be3cc65b7c177a74752ddc
ThibulskiySergey <c4r0n0s@gmail.com>
2016-03-01 17:43:40 +0200

    Add minify js

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,12 +4,14 @@ var gulp   = require('gulp');
 var watch  = require('gulp-watch');
 var coffee = require('gulp-coffee');
 var sass   = require('gulp-sass');
+var uglify = require('gulp-uglify');
 
 gulp.task('development', function() {
   gulp.src('./src/coffee/*.coffee')
     .pipe(watch('./src/coffee/*.coffee'))
     .pipe(coffee({ bare: true }))
-    .pipe(gulp.dest('./dist/js'));
+    //.pipe(uglify())
+    .pipe(gulp.dest('./dist/source'));
 
   gulp.src('./src/scss/*.scss')
     .pipe(watch('./src/scss/*.scss'))
